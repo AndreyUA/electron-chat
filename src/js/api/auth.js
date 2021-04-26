e1be5ea22f895ce60573a3b249ae385b1f5a9030
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-04-26 16:22:39 +0300

    added profiles

--- src/js/api/auth.js
+++ src/js/api/auth.js
@@ -1,11 +1,26 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-export async function register({ email, password }) {
+import db from "../db/firestore";
+
+const createUserProfile = (userProfile) =>
+  db.collection("profiles").doc(userProfile.uid).set(userProfile);
+
+export async function register({ email, password, username, avatar }) {
   try {
     const { user } = await firebase
       .auth()
       .createUserWithEmailAndPassword(email, password);
+
+    const userProfile = {
+      uid: user.uid,
+      username,
+      email,
+      avatar,
+      joinedChats: [],
+    };
+
+    await createUserProfile(userProfile);
 
     return user;
   } catch (error) {
