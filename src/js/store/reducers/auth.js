02ea73185fe4552a9e1c68d25e56ae073ec395e2
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-04-27 11:25:21 +0300

    refactored auth reducer

--- src/js/store/reducers/auth.js
+++ src/js/store/reducers/auth.js
@@ -1,3 +1,6 @@
+import { combineReducers } from "redux";
+
+// Constants
 import {
   AUTH_ON_INIT,
   AUTH_ON_SUCCESS,
@@ -5,54 +8,45 @@ import {
   AUTH_LOGOUT_SUCCESS,
   AUTH_LOGIN_INIT,
   AUTH_REGISTER_INIT,
-  AUTH_LOGIN_SUCCESS,
-  AUTH_REGISTER_SUCCESS,
 } from "../actions/types";
 
-const initialState = {
-  user: null,
-  isChecking: false,
+const createAuthReducer = () => {
+  const user = (state = null, action) => {
+    const { payload, type } = action;
+
+    switch (type) {
+      case AUTH_ON_ERROR:
+      case AUTH_ON_INIT:
+      case AUTH_LOGOUT_SUCCESS:
+        return null;
+      case AUTH_ON_SUCCESS:
+        return payload;
+      default:
+        return state;
+    }
+  };
+
+  const isChecking = (state = false, action) => {
+    const { type } = action;
+
+    switch (type) {
+      case AUTH_LOGIN_INIT:
+      case AUTH_REGISTER_INIT:
+      case AUTH_ON_INIT:
+        return true;
+      case AUTH_ON_SUCCESS:
+      case AUTH_ON_ERROR:
+      case AUTH_LOGOUT_SUCCESS:
+        return false;
+      default:
+        return state;
+    }
+  };
+
+  return combineReducers({
+    user,
+    isChecking,
+  });
 };
 
-export default function authReducer(state = initialState, action) {
-  const { payload, type } = action;
-
-  switch (type) {
-    case AUTH_LOGIN_INIT:
-    case AUTH_REGISTER_INIT:
-      return {
-        ...state,
-        isChecking: true,
-      };
-    case AUTH_LOGIN_SUCCESS:
-    case AUTH_REGISTER_SUCCESS:
-      return {
-        ...state,
-        isChecking: false,
-      };
-    case AUTH_ON_INIT:
-      return {
-        ...state,
-        isChecking: true,
-      };
-    case AUTH_ON_SUCCESS:
-      return {
-        ...state,
-        user: payload,
-        isChecking: false,
-      };
-    case AUTH_ON_ERROR:
-      return {
-        ...state,
-        isChecking: false,
-      };
-    case AUTH_LOGOUT_SUCCESS:
-      return {
-        ...state,
-        user: null,
-        isChecking: false,
-      };
-    default:
-      return state;
-  }
-}
+export default createAuthReducer();
