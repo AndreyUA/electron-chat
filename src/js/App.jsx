655c1da770e793bc52fc303abf820f8e88bb753b
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-04-27 15:17:54 +0300

    refactored app component

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,9 +1,9 @@
 // Progress
-// 4 __dirname && 18 __filename
+// 4 __dirname && 19 __filename
 
 import React, { useEffect } from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
 
 // Redux
 import configueStore from "./store";
@@ -15,31 +15,45 @@ import Home from "./views/Home.jsx";
 import Chat from "./views/Chat.jsx";
 import Settings from "./views/Settings.jsx";
 import Welcome from "./views/Welcome.jsx";
+import LoadingView from "./components//shared/LoadingView.jsx";
 
 // Components
 import NavBar from "./components/NavBar.jsx";
 
 // React component
-const App = () => {
+const ChatApp = () => {
+  const dispatch = useDispatch();
+
+  const isChecking = useSelector(({ auth }) => auth.isChecking);
+
   useEffect(() => {
-    store.dispatch(listenToAuthChanges());
+    dispatch(listenToAuthChanges());
   }, []);
 
+  if (isChecking) {
+    return <LoadingView />;
+  }
+
   return (
-    <Provider store={store}>
-      <Router>
-        <NavBar />
-        <div className="content-wrapper">
-          <Switch>
-            <Route path="/" component={Welcome} exact />
-            <Route path="/home" component={Home} />
-            <Route path="/settings" component={Settings} />
-            <Route path="/chat/:id" component={Chat} />
-          </Switch>
-        </div>
-      </Router>
-    </Provider>
+    <Router>
+      <NavBar />
+      <div className="content-wrapper">
+        <Switch>
+          <Route path="/" component={Welcome} exact />
+          <Route path="/home" component={Home} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/chat/:id" component={Chat} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
+
+// React component
+const App = () => (
+  <Provider store={store}>
+    <ChatApp />
+  </Provider>
+);
 
 export default App;
