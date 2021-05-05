a92e86a0bd44ba983a67ae0dda9d306f66d6039a
Andrey Zhygarev <andrii.zhygarev@dstarlab.com>
2021-05-05 14:22:29 +0300

    fixed menu

--- utils/Menu.js
+++ utils/Menu.js
@@ -28,6 +28,34 @@ exports.createTemplate = (app) => {
           // More here: https://www.electronjs.org/docs/api/webview-tag#methods
           role: "undo",
         },
+        {
+          label: "Redo",
+          accelerator: "Shift+CmdOrCtrl+Z",
+          selector: "redo:",
+        },
+        {
+          type: "separator",
+        },
+        {
+          label: "Cut",
+          accelerator: "CmdOrCtrl+X",
+          selector: "cut:",
+        },
+        {
+          label: "Copy",
+          accelerator: "CmdOrCtrl+C",
+          selector: "copy:",
+        },
+        {
+          label: "Paste",
+          accelerator: "CmdOrCtrl+V",
+          selector: "paste:",
+        },
+        {
+          label: "Select All",
+          accelerator: "CmdOrCtrl+A",
+          selector: "selectAll:",
+        },
       ],
     },
     {
